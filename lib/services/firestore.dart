2fbb7d6a0803a6e84716aa46dea9b1abb80ba13d
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-23 16:22:34 +0100

    feat: Created services folder which contains a firestoredart file - Added FirestoreService class to manage notes in Firestore. - Implemented addNote method to create new notes in the 'notes' collection. - Included comments for future implementation of READ, UPDATE, and DELETE methods.

---
+++ lib/services/firestore.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  // get notes collection from DB
+  final CollectionReference notes =
+      FirebaseFirestore.instance.collection("notes");
+
+  // CREATE: add a new note
+  Future<void> addNote(String newNote) {
+    return notes.add({"notes": newNote, "timestamp": Timestamp.now()});
+  }
+  // READ: get notes from Database
+
+  // UPDATE: update notes given a doc id
+
+  // DELETE: delete notes given a doc id
+}
