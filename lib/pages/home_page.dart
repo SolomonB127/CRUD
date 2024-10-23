c8b4f4425769453d59759c9c612d81b2a245a1d4
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-23 16:24:14 +0100

    feat: Added TextField for inputting notes and connected to FirestoreService for saving. - - Included functionality to clear the text input and close the dialog after saving a note.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,46 @@
+import 'package:firestore_db/services/firestore.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // firestore object
+  final FirestoreService firestoreService = FirestoreService();
+
+  // text controller
+  final TextEditingController textController = TextEditingController();
+
+//  open dialog method
+  void openNoteDialog() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: textController,
+              ),
+              actions: <Widget>[
+                // save button
+                ElevatedButton(
+                  onPressed: () {
+                    // add a new note
+                    firestoreService.addNote(textController.text);
+
+                    // clear controller
+                    textController.clear();
+
+                    // pop dialog
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Save"),
+                )
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +53,7 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: openNoteDialog,
         child: const Icon(Icons.add),
       ),
     );
